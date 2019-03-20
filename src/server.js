5a2b940fb366112776c288052c57df0872879b3c
liujiandada <1477716853@qq.com>
2019-03-20 10:53:39 +0800

    更新依赖

--- src/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-function service(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Node.js');
-}
-var server = http.createServer(service);
-server.listen(8088);
